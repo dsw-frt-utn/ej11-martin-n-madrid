29f83d0fa2261311936c5598d3962f4a6e050ae1
Martin Madrid <martin.madrid@alu.frt.utn.edu.ar>
2026-05-22 20:43:10 -0300

    Se completo el CasoDictionary

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un diccionario donde la clave sea el legajo y el valor el alumno
 //Incluir un método para agregar un alumno al diccionario
@@ -7,4 +9,57 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+    public Dictionary<int, Alumno> Alumnos { get; }
+
+    public CasoDictionary()
+    {
+        Alumnos = new Dictionary<int, Alumno>();
+    }
+
+    public void AgregarAlumno(int legajo, Alumno alumno)
+    {
+        try
+        {
+            Alumnos.Add(legajo, alumno);
+        }
+        catch (ArgumentException aEx)
+        {
+            Console.WriteLine("El legajo ya existe en el diccionario." + aEx.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error inesperado." + ex.Message);
+        }
+    }
+
+    public Alumno? buscarAlumno(int legajo)
+    {
+        try
+        {
+            return Alumnos[legajo];
+        }
+        catch (KeyNotFoundException knfEx)
+        {
+            Console.WriteLine($"No se encontró alumno con legajo {legajo}. Error: {knfEx.Message}");
+            return null;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error inesperado." + ex.Message);
+            return null;
+        }
+    }
+
+    public void eliminarAlumno(int legajo)
+    {
+        if (Alumnos.ContainsKey(legajo))
+        {
+            Alumnos.Remove(legajo);
+            Console.WriteLine("Alumno eliminado");
+        }
+        else
+        {
+            Console.WriteLine("No se encontró al alumno con legajo: " + legajo);
+        }
+    }
 }
--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,8 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+using System.Numerics;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
@@ -21,12 +25,35 @@ internal class Ejemplos
     //Eliminar un alumno por clave y listar por consola los alumnos
     public static void EjemploDictionary()
     {
+        var diccionario = new CasoDictionary();
+        diccionario.AgregarAlumno(1000, new Alumno(11, "Juan", 8.5));
+        diccionario.AgregarAlumno(2000, new Alumno(12, "Maria", 7.3));
+        diccionario.AgregarAlumno(2500, new Alumno(13, "Celeste", 7.9));
 
+        buscarAlumnosDiccionario(diccionario.Alumnos);
+
+        Console.WriteLine(diccionario.buscarAlumno(1000));
+        Console.WriteLine(diccionario.buscarAlumno(3000));
+
+        diccionario.eliminarAlumno(2500);
+
+        buscarAlumnosDiccionario(diccionario.Alumnos);
     }
 
     //Realizar una llamada a cada método definido en CasoLinq y mostar por consola según corresponda
     public static void EjemploLinq()
     {
 
+    }
+
+    public static void buscarAlumnosDiccionario(Dictionary<int, Alumno> alumnos)
+    {
+        foreach (KeyValuePair<int, Alumno> elemento in alumnos)
+        {
+            int legajo = elemento.Key;
+            Alumno alumnoActual = elemento.Value;
+
+            Console.WriteLine($"Legajo: {legajo}, Nombre: {alumnoActual.Nombre}, Promedio: {alumnoActual.Promedio}");
+        }
     }
 }
